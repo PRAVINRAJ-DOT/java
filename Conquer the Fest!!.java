f978efdc1bbe7a03598fb13810954122d936e261
PRAVINRAJ-DOT <pravinraj6167@gmail.com>
2026-07-22 16:17:17 +0530

    Add files via upload

---
+++ Conquer the Fest!!.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if (a>=10*b){
+		    System.out.print("YES");
+		} 
+		else{
+		    System.out.print("NO");
+		}
+
+	}
+}
