5548d65abeb777a9e3805621144025dd7241dac3
PRAVINRAJ-DOT <pravinraj6167@gmail.com>
2026-07-22 16:05:13 +0530

    Add files via upload

---
+++ VACAEX.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt(),b=sc.nextInt();
+		System.out.print(a+b-1);
+		
+
+	}
+}
